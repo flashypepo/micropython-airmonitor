90148d5413c9e3fa4cfe226b0488fcf234930ad0
Peter <peter@pepo.nl>
2023-09-22 23:07:10 +0200

    add emergency buffer
    Future usage.

--- boot.py
+++ boot.py
@@ -2,6 +2,9 @@
 This file is executed on every boot (including wake-boot from deepsleep)
 
 """
+import micropython
+micropython.alloc_emergency_exception_buf(100)
+
 #import esp
 #esp.osdebug(None)
 #import webrepl
